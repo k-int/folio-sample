ad2aa897830bce352d868ec5d4e339a501e217fb
Marc Johnson <marc@k-int.co.uk>
2016-08-16 14:23:34 +0100

    Build all modules with a single command (rather than two).

--- start-modules.sh
+++ start-modules.sh
@@ -1,15 +1,8 @@
-cd first-module
+#!/usr/bin/env bash
 
 gradle fatJar
 
-Java -jar build/libs/first-module.jar 1>first-module.log 2>first-module.log &
+Java -jar first-module/build/libs/first-module.jar 1>first-module.log 2>first-module.log &
 
-cd ..
+Java -jar second-module/build/libs/second-module.jar 1>second-module.log 2>second-module.log &
 
-cd second-module
-
-gradle fatJar
-
-Java -jar build/libs/second-module.jar 1>second-module.log 2>second-module.log &
-
-cd ..
