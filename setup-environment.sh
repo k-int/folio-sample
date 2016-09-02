1a1629a99f00f3b9ee4f58d2ee71997f06d49e36
Marc Johnson <marc@k-int.co.uk>
2016-09-02 10:53:17 +0100

    Container instances have names for easy identification

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,2 +1,2 @@
-sdk use gradle 3.0-rc-2
+sdk use gradle 3.0
 sdk use groovy 2.4.7
--- stop-modules-docker.sh
+++ stop-modules-docker.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+#docker ps | grep "[first|second]-module" | awk '{print $1}' | docker stop
 docker stop `docker ps | grep "[first|second]-module" | awk '{print $1}'`
+docker rm `docker ps -a | grep "[first|second]-module" | awk '{print $1}'`
